db5d004ec18c5370deccf41658e558214f1ecabf
Caiozatski <caiozatroski123@gmail.com>
2026-08-02 15:12:19 -0300

    Add sound effects to door and light controls

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-//Light door button
+let homeContent = document.querySelector(".home-main-content");
+
+//Light door button function
 
 function turnLights(){
   homeContent.classList.toggle("lights-on");
   lightButton.classList.toggle("light-panel-on")
+  if (lightButton.classList.contains("light-panel-on")){
+    lightSound.play()
+  } else{
+    lightSound.pause()
+  }
+
 }
+
+let lightButton = document.querySelector(".light-button");
+lightButton.addEventListener("click", turnLights);
+let lightSound = new Audio("/FnafFan/assets/sounds/lights.wav")
+
+// Door close/open button functions
 
 function doorClose(){
   doorButton.classList.toggle("door-panel-on");
   doorImage.classList.toggle("door-close");
+  doorSound.currentTime = 0;
+  doorSound.play()
 }
 
 let doorImage = document.querySelector(".door-image");
 
-let homeContent = document.querySelector(".home-main-content");
-
-let lightButton = document.querySelector(".light-button");
-lightButton.addEventListener("click", turnLights);
-
-
 let doorButton = document.querySelector(".door-button");
 doorButton.addEventListener("click", doorClose)
+let doorSound = new Audio("/FnafFan/assets/sounds/door.wav")
