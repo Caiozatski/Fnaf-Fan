cb96821aed0863f16d8f7b392379df1b7196eb95
Caiozatski <caiozatroski123@gmail.com>
2026-08-02 14:38:05 -0300

    Added door open/close system

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 //Light door button
 
 function turnLights(){
-  console.log("acendeu");
   homeContent.classList.toggle("lights-on");
   lightButton.classList.toggle("light-panel-on")
 }
 
+function doorClose(){
+  doorButton.classList.toggle("door-panel-on");
+  doorImage.classList.toggle("door-close");
+}
+
+let doorImage = document.querySelector(".door-image");
+
+let homeContent = document.querySelector(".home-main-content");
+
 let lightButton = document.querySelector(".light-button");
-  let homeContent = document.querySelector(".home-main-content");
-  lightButton.addEventListener("click", turnLights);
+lightButton.addEventListener("click", turnLights);
+
+
+let doorButton = document.querySelector(".door-button");
+doorButton.addEventListener("click", doorClose)
